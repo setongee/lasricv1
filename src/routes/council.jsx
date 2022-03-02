99841de518936ab03816c0fd6151f02a249357f7
Setonji <setongee@gmail.com>
2022-03-02 23:22:43 +0100

    removed Lorem Ipsum

--- src/routes/council.jsx
+++ src/routes/council.jsx
@@ -8,8 +8,6 @@ const Council = () => {
 
             <iframe src="https://lasric.lagosstate.gov.ng/people" frameBorder="0"></iframe>
 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis reiciendis magnam beatae obcaecati pariatur voluptas nam velit nesciunt quos harum vel, nisi debitis eveniet accusamus enim odit. Deleniti distinctio officiis autem pariatur, ut minima omnis placeat perspiciatis molestiae nihil aliquam ad illum deserunt. Eligendi aut quidem obcaecati, expedita rerum consectetur repudiandae! Tempore, minima ipsa? Ratione, atque magni! Repellat adipisci quidem nobis dignissimos deserunt tempore, hic earum expedita, non animi illum obcaecati cupiditate neque possimus sit ea blanditiis delectus tenetur suscipit porro eos? Vel laborum exercitationem cumque modi nobis cupiditate neque consectetur aliquid deserunt atque expedita eligendi, adipisci provident amet inventore!
-
         </div>
     );
 }
